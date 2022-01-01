aa9af8a71f14e5e8fea4cc1120a8635caccf35dd
Joe <joe@coder.com>
2022-01-01 05:43:01 +0000

    wip: add failing test for streakCounter

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,5 +1,27 @@
-describe("basic test", () => {
-    it("should pass", () => {
-        expect(true).toBe(true)
-    })
-})
+import { JSDOM } from "jsdom";
+import { streakCounter } from "../src/index";
+
+describe("streakCounter", () => {
+  let mockLocalStorage: Storage;
+
+  beforeEach(() => {
+    const mockJSDom = new JSDOM("", { url: "https://localhost" });
+
+    mockLocalStorage = mockJSDom.window.localStorage;
+  });
+
+  it("should return a 'streak' object with currentCount, starteDate and lastLoginDate", () => {
+    const date = new Date();
+    const streak = streakCounter(mockLocalStorage, date);
+
+    expect(Object.prototype.hasOwnProperty.call(streak, "currentCount")).toBe(
+      true
+    );
+    expect(Object.prototype.hasOwnProperty.call(streak, "startDate")).toBe(
+      true
+    );
+    expect(Object.prototype.hasOwnProperty.call(streak, "lastLoginDate")).toBe(
+      true
+    );
+  });
+});
