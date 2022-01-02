42b27f1915f4dcc35cb66a7ddd1a2065876161bf
Joe <joe@coder.com>
2022-01-02 04:58:05 +0000

    wip: prepopulated streak

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -46,4 +46,34 @@ describe("streakCounter", () => {
     const streakAsString = mockLocalStorage.getItem(key);
     expect(streakAsString).not.toBeNull();
   });
+  describe("with a prepopulated streak", () => {
+    let mockLocalStorage: Storage;
+    beforeEach(() => {
+      const mockJSDom = new JSDOM("", { url: "https://localhost" });
+
+      mockLocalStorage = mockJSDom.window.localStorage;
+
+      // Use date in past so it doesn't
+      // conflict if test uses date test runs.
+      const date = new Date("2021-12-12");
+
+      const streak = {
+        currentCount: 1,
+        startDate: formattedDate(date),
+        lastLoginDate: formattedDate(date),
+      };
+
+      mockLocalStorage.setItem("streak", JSON.stringify(streak));
+    });
+    afterEach(() => {
+      mockLocalStorage.clear();
+    });
+    it("should return the streak from localStorage", () => {
+      const date = new Date();
+      const streak = streakCounter(mockLocalStorage, date);
+
+      // Should match the dates used to set up the tests
+      expect(streak.startDate).toBe("12/12/2021");
+    });
+  });
 });
