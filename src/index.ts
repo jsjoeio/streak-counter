aef88a89586834b68ea4dd2ec94baac5f27c062b
Joe <joe@coder.com>
2022-01-02 04:38:07 +0000

    feat: store in localStorage

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,18 @@ interface Streak {
   lastLoginDate: string;
 }
 
+// Used when storing in localStorage
+const KEY = "streak";
+
 export function streakCounter(_localStorage: Storage, date: Date): Streak {
-  return {
+  const streak = {
     currentCount: 1,
     startDate: formattedDate(date),
     lastLoginDate: formattedDate(date),
   };
+
+  // store in localStorage
+  _localStorage.setItem(KEY, JSON.stringify(streak));
+
+  return streak;
 }
